a1b1df237be777c284d27395c3c7332650d5e88f
wulimammoth <ctelestial@gmail.com>
2024-07-02 19:34:09 -0700

    Actually working menu! Weird looking though...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,69 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NgxRadialMenuComponent,MenuConfig} from "../../../ngx-radial-menu/dist/ngx-radial-menu";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NgxRadialMenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  @ViewChild('menuElement') menuElement?: NgxRadialMenuComponent;
   title = 'ngx-radial-menu-demo';
+
+  show(event: MouseEvent) {
+    // console.log(this.menuElement);
+    if (this.menuElement) {
+      this.menuElement.show({x: event.clientX, y: event.clientY});
+      console.log(this.menuElement.coordinates);
+    }
+    event.preventDefault();
+  }
+  config: Partial<MenuConfig> = {
+    background: '#99AABBCC',
+    diameter: 300,
+    menus: [
+      {
+        title: "GitHub",
+        icon: {fontIcon: "search"},
+        href: "http://github.com",
+        target: "_blank"
+      }, {
+        title: "GitLab",
+        icon: {
+          fontIcon: "star",
+          color: '#4078c0'
+        },
+      }, {
+        title: "subMenu",
+        icon: {fontIcon: "chevron_right"},
+        menus: [{
+          title: 'subMenu1',
+          icon: {fontIcon: 'bolt'}
+        }, {
+          title: 'subMenu2',
+          icon: {fontIcon: 'file'}
+        }]
+      }, {
+        title: "subMenu",
+        icon: {fontIcon: 'sort'}
+      }, {
+        title: "click",
+        icon: {fontIcon: 'reply'}
+      }, {
+        title: "hash-href",
+        href: "#someHash",
+      }, {
+        title: "clickMe!",
+        click: function () {
+          alert('click event callback');
+        }
+      }, {
+        disabled: true,
+        title: "disabled"
+      }
+    ]
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,12 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {NgxRadialMenuService} from "../../../ngx-radial-menu/dist/ngx-radial-menu";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    NgxRadialMenuService
+  ]
 };
